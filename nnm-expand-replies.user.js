21a4c4c35eb60e782ce6df3d116a6e2e2857d2f6
T1mL3arn <9349164+T1mL3arn@users.noreply.github.com>
2019-01-29 21:50:53 +1000

    add gplv3 notices

--- nnm-expand-replies.user.js
+++ nnm-expand-replies.user.js
@@ -1,3 +1,21 @@
+/* 
+    NoName Club expand replies - Adds a button which opens topic's replies.
+    Copyright (C) 2019  T1mL3arn
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>. 
+*/
+
 // ==UserScript==
 // @name NoName Club expand replies
 // @namespace https://github.com/T1mL3arn
